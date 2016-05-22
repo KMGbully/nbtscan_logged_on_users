d5114a28d40e8d8368b696181bac9a76b127de7a
KMGbully <kevin.gilstrap@att.net>
2016-05-22 14:43:28 -0500

    Update

--- logged_on_users.sh
+++ logged_on_users.sh
@@ -3,5 +3,11 @@ if [ -z "$1" ]; then
   echo "[*] Usage:  ./logged_on_users.sh <filename with ip addresses>"
   exit 0
 fi
-for scan in $(cat $1); do results=$(nbtscan -vq $scan | grep "03"); if [ ! -z "$results" ]; then user=$(echo $results | awk '{print $4}') && pc=$(echo $results | awk '{print $1}'); echo User $user logged onto $pc; fi; done
-
+for scan in $(cat $1); do 
+  results=$(nbtscan -vq $scan | grep "03" | awk '{print $1}')
+ if [ ! -z "$results" ]; then
+   pc=$(host $results | grep "has address" | awk '{print $4}')
+   user=$(nbtscan -q $pc | awk '{print $4}')
+   echo $(tput setaf 3)User $user logged onto $pc $(tput setaf 7)
+ fi
+done
